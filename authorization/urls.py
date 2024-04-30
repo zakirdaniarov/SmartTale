3cb7f0846b04faf376e174b7ef7d84fc3bd9a621
nkozhogulov <kozhogulovn00@gmail.com>
2024-05-01 01:56:14 +0300

    T2-74-fix-authorization Delete http-only

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import (SignupAPIView, LoginAPIView, LogoutAPIView, CookieTokenRefreshView,
-                    VerifyEmailAPIView, ResendEmailVerificationCodeAPIView)
+from .views import (SignupAPIView, LoginAPIView, LogoutAPIView, TokenRefreshView, 
+                    DeleteUserAPIView, VerifyEmailAPIView, ResendEmailVerificationCodeAPIView)
 
 urlpatterns = [
     path('authorization/registration', SignupAPIView.as_view(), name = 'authorization-registration'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('authorization/verify-email', VerifyEmailAPIView.as_view(), name = 'authorization-verify-email'),
     path('authorization/resend-code', ResendEmailVerificationCodeAPIView.as_view(), name = 'authorization-resend-code'),
     path('authorization/logout', LogoutAPIView.as_view(), name = 'authorization-logout'),
-    path('authorization/refresh-token', CookieTokenRefreshView.as_view(), name = 'authorization-refresh-token'),
+    path('authorization/refresh-token', TokenRefreshView.as_view(), name = 'authorization-refresh-token'),
+    path('authorization/delete-user', DeleteUserAPIView.as_view(), name = 'authorization-delete-user'),
 ]
