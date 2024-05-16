6f3518191936a856b3851889b0750317e3846f12
Ular <raevular01@gmail.com>
2024-05-16 20:56:35 +0600

    implemented CRUD for vacancy and resume

--- job/views.py
+++ job/views.py
@@ -1,20 +1,22 @@
 from rest_framework import views, status, permissions
 from rest_framework.response import Response
 
+from authorization.models import Organization
 from .models import Vacancy, Resume
-from .serializers import VacancySerializer, ResumeSerializer
+from .serializers import (VacancyListSerializer, VacancyDetailSerializer,
+                          ResumeListSerializer, ResumeDetailSerializer)
 from .permissions import CurrentUserOrReadOnly
 
 
 class VacancyListAPIView(views.APIView):
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         try:
             vacancy = Vacancy.objects.all().order_by('-created_at')
         except Vacancy.DoesNotExist:
             return Response({"error": "Vacancy does not exist"}, status=status.HTTP_404_NOT_FOUND)
-        serializer = VacancySerializer(vacancy, many=True)
+        serializer = VacancyListSerializer(vacancy, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -22,10 +24,14 @@ class AddVacancyAPIView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        serializer = VacancySerializer(data=request.data)
+        organization = Organization.objects.filter(owner=request.user.user_profile).first()
+
+        if not organization:
+            return Response({"error": "You must belong to an organization to create a vacancy"},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        serializer = VacancyDetailSerializer(data=request.data)
         if serializer.is_valid():
-            current_organization = request.user.user_profile.current_member
-            organization = serializer.validated_data['organization'] = current_organization
             serializer.save(organization=organization)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -35,7 +41,21 @@ class ChangeVacancyAPIView(views.APIView):
     permission_classes = [CurrentUserOrReadOnly]
 
     def put(self, request, *args, **kwargs):
-        ...
+        try:
+            vacancy = Vacancy.objects.get(slug=kwargs['vacancy_slug'])
+        except Vacancy.DoesNotExist:
+            return Response({"error": "Vacancy does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        organization = Organization.objects.filter(owner=request.user.user_profile).first()
+
+        if not organization:
+            return Response({"error": "Only organization that added it can change"}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = VacancyDetailSerializer(instance=vacancy, data=request.data)
+        if serializer.is_valid():
+            serializer.save(organization=organization)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DeleteVacancyAPIView(views.APIView):
@@ -47,11 +67,66 @@ class DeleteVacancyAPIView(views.APIView):
         except Vacancy.DoesNotExist:
             return Response({'error': "Vacancy does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-        organization = request.user
+        current_organization = Organization.objects.filter(owner=request.user.user_profile).first()
 
-        if organization == 'org_vacancy':
+        if vacancy.organization == current_organization:
             vacancy.delete()
         else:
             return Response({"error": "Only organization that added it can delete"}, status=status.HTTP_400_BAD_REQUEST)
         return Response({"error": "Successfully deleted"}, status=status.HTTP_200_OK)
 
+
+class ResumeListAPIView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            resume = Resume.objects.all().order_by('-created_at')
+        except Resume.DoesNotExist:
+            return Response({"error": "Resume does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ResumeListSerializer(resume, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AddResumeAPIView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = ResumeDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            author = request.user.user_profile
+            serializer.save(author=author)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangeResumeAPIView(views.APIView):
+    permission_classes = [CurrentUserOrReadOnly]
+
+    def put(self, request, *args, **kwargs):
+        try:
+            resume = Resume.objects.get(slug=kwargs['resume_slug'])
+        except Resume.DoesNotExist:
+            return Response({"error": "Resume does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ResumeDetailSerializer(instance=resume, data=request.data)
+        if serializer.is_valid():
+            author = request.user.user_profile
+            serializer.save(author=author)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteResumeAPIView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            resume = Resume.objects.get(slug=kwargs['resume_slug'])
+        except Resume.DoesNotExist:
+            return Response({"error": "Resume does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        author = request.user.user_profile
+
+        if resume.author == author:
+            resume.delete()
+        else:
+            return Response({"error": "Only author can delete"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "Successfully deleted"}, status=status.HTTP_200_OK)
