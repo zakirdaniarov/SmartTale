cd274d60134945b04334a20831c79ab8c77bd7ba
nkozhogulov <kozhogulovn00@gmail.com>
2024-06-18 02:07:15 +0300

    Fix asgi

--- smarttale/asgi.py
+++ smarttale/asgi.py
@@ -10,17 +10,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smarttale.settings")
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
+
 import chat.routing
 from chat.channelsmiddleware import JwtAuthMiddlewareStack
 
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             JwtAuthMiddlewareStack(
                 URLRouter(
