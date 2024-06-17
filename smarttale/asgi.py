4e5bc73ccd2ebd31d92f2583bef2bd8d43d3d59c
nkozhogulov <kozhogulovn00@gmail.com>
2024-06-18 02:10:26 +0300

    Fix asgi

--- smarttale/asgi.py
+++ smarttale/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smarttale.settings")
 django_asgi_app = get_asgi_application()
 
 import chat.routing
