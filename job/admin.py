c924e1e6770394ca623a7b4325db5afeff07554c
nkozhogulov <kozhogulovn00@gmail.com>
2024-06-20 00:57:48 +0300

    Fix some bugs

--- job/admin.py
+++ job/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Resume, Vacancy
+from .models import Resume, Vacancy, VacancyResponse
 
 admin.site.register(Resume)
 admin.site.register(Vacancy)
+admin.site.register(VacancyResponse)
